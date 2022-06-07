9f9e9185ce4b1d631511220e25b80ceacb89e69c
Akhilesh-2410 <74154584+Akhilesh-2410@users.noreply.github.com>
2022-06-08 00:33:13 +0530

    Add files via upload

---
+++ backend/routes/middleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const JWT_Secret="edwhfkwoddjfoiwdjfoijwdofijww";
+const mongoose=require("mongoose")
+const user=mongoose.model("User");
+module.exports=(req,res,next)=>{
+    const{authorization}=req.headers;
+    if(!authorization)
+        return res.status(401).json({error:"you must be logged in"});
+    const token=authorization.replace("Bearer ","");
+    jwt.verify(token,JWT_Secret,(err,payload)=>{
+        if(err)
+            return res.status(401).json({error:"you must be logged in"});
+        const{_id}=payload;
+        user.findById(_id).then(user=>{
+            req.user=user;
+            next();
+        })
+    })
+}
