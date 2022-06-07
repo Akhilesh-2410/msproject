ab44d9e3f769ee99ec724dbbbc57a039d8d1688b
Akhilesh-2410 <74154584+Akhilesh-2410@users.noreply.github.com>
2022-06-08 01:58:09 +0530

    Add files via upload

---
+++ backend/routes/userdata.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router=express.Router();
+const mongoose = require('mongoose');
+const requirelogin = require('./middleware.js');
+const userpost=mongoose.model("UserPosts");
+
+
+router.post("/postdoc",requirelogin,(req,res)=>{
+    const{userid,requirementType,data}=req.body;
+    if(!data||!requirementType)
+        return res.status(422).json({error:"please add all of the fields"});
+    const post=new userpost({
+        userid,
+        requirementType,
+        data
+    })
+    post.save().then(result=>{
+        res.json({post:result});
+    }).catch(err=>{
+        console.log(err);
+    })
+})
+
+
+router.put("/updatedoc",requirelogin,(req,res)=>{
+    const {requirementType,data}=req.body;
+    userpost.findByIdAndUpdate(req.body._id,{
+        $set:{requirementType:requirementType,
+        data:data}
+    },{new:true}).exec((err,result)=>{
+        if(err)
+            return res.status(422).json({error:err})
+        else
+            res.json(result);
+    })
+})
+module.exports=router;
