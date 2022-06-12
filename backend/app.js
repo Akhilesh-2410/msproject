a24abe90669f2eeae1059179e80ce7c96e4e2bdf
tgashwinkumar <tgashwinkumar@gmail.com>
2022-06-12 11:36:21 +0530

    Adding file upload

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+const multer = require("multer");
+
+const fileUpload = multer();
+
 const mongouri =
   "mongodb+srv://msproject:msprojects5@cluster0.crroi.mongodb.net/?retryWrites=true&w=majority";
 const app = express();
@@ -18,9 +24,34 @@ mongoose.connect(mongouri);
 mongoose.connection.on("connected", () => {
   console.log("we are connected to db");
 });
+
 app.get("/", (req, res) => {
   res.send("Helooo broooooo");
 });
+
+app.post("/upload", fileUpload.single("file"), (req, res, next) => {
+  let streamUpload = (req) => {
+    return new Promise((resolve, reject) => {
+      let stream = cloudinary.uploader.upload_stream((error, result) => {
+        if (result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      });
+
+      streamifier.createReadStream(req.file.buffer).pipe(stream);
+    });
+  };
+
+  async function upload(req) {
+    let result = await streamUpload(req);
+    console.log(result);
+  }
+
+  upload(req);
+});
+
 app.listen(5000, (req, res) => {
   console.log("Server is running at 5000");
 });
