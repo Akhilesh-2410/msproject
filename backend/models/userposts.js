371eba400e2817f2b9fedd30d5c3c28021c8caab
Akhilesh-2410 <74154584+Akhilesh-2410@users.noreply.github.com>
2022-06-13 16:57:04 +0530

    Update userposts.js

--- backend/models/userposts.js
+++ backend/models/userposts.js
@@ -1,25 +1,32 @@
-const mongoose = require("mongoose");
-const { ObjectId } = mongoose.Schema.Types;
-const userpostschema = new mongoose.Schema({
-  userid: {
-    type: String,
-    required: true,
-  },
-  requirementType: {
-    type: String,
-    required: true,
-  },
-  data: [
+const mongoose = require('mongoose');
+const {ObjectId} =mongoose.Schema.Types;
+const userpostschema=new mongoose.Schema(
     {
-      name: {
-        type: String,
-        required: true,
-      },
-      url: {
-        type: String,
-        required: true,
-      },
-    },
-  ],
-});
-module.exports = mongoose.model("UserPosts", userpostschema);
+        userid:{
+            type:String,
+            required:true
+        },
+        requirementType:{
+            type:String,
+            required:true
+        },
+        data:{
+            type:Object,
+            required:true
+        },
+        document:[
+            {
+                requirementType:{
+                    type:String
+                },
+                name:{
+                    type:String
+                },
+                url:{
+                    type:String
+                    }
+            }
+        ]
+    }
+)
+module.exports=mongoose.model("UserPosts",userpostschema);
